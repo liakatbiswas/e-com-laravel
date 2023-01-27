1e5062d757aba4c17a65b0e703f7b6f92c5e9bb1
liakatbiswas <87108284+liakatbiswas@users.noreply.github.com>
2023-01-28 00:16:48 +0600

    Header, Footer & Login form done!

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,27 @@
+<footer class="bg-dark text-white py-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h4>About Us</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor, magna id congue fermentum, ipsum
+                    ipsum tincidunt augue, a lobortis risus ligula a elit.</p>
+            </div>
+            <div class="col-md-4">
+                <h4>Contact Us</h4>
+                <ul class="list-unstyled">
+                    <li>Email: info@example.com</li>
+                    <li>Phone: 555-555-5555</li>
+                    <li>Address: 123 Main St, Anytown USA</li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <h4>Follow Us</h4>
+                <ul class="list-unstyled">
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Instagram</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Disabled</a>
+                </li>
+            </ul>
+            <ul class="d-flex">
+                <li><a href="#">Cart(0)</a></li>
+            </ul>
+
+        </div>
+    </div>
+</nav>
